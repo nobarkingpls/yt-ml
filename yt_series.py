307591e99db1f1735b3e3e175fb4c1d4d22fbd39
Judith Green <ajudithgreen@gmail.com>
2025-02-28 10:06:42 -0800

    fix bug in series

--- yt_series.py
+++ yt_series.py
@@ -205,8 +205,8 @@ def series_mec():
                     match = re.search(r' id=.av_genre_.*/>', filedata).group(0)
                     genre_input = input('Enter a genre (eg. Comedy, Drama, Reality, Documentary): ')
                     filedata = filedata.replace(match, f'>{genre_input}</md:Genre>\n<md:Genre>Gay Lesbian</md:Genre>')
-                    except:
-                        pass
+            except:
+                pass
 
             #check empty year
             try:
